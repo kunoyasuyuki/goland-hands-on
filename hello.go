9e9f463d8d2dc20d91835e1cc6feb08febb9f633
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-10-28 20:53:56 +0900

    change seasons

--- hello.go
+++ hello.go
@@ -8,15 +8,20 @@ import (
 
 func main() {
         x := hello.Input("type a number")
-        n, err := strconv.Atoi(x)
-        if err != nil {
-                fmt.Println("ERROR!!")
-                return
-        }
-        fmt.Print(x + "は、")
-        if n%2 == 0 {
-                fmt.Println("偶数です。")
-        } else {
-                fmt.Println("奇数です。")
+        fmt.Print(x + "月は、")
+        switch n, err := strconv.Atoi(x); n {
+        case 0:
+                fmt.Println("整数値が得られません。")
+                fmt.Println(err)
+        case 1, 2, 12:
+                fmt.Println("冬です。")
+        case 3, 4, 5:
+                fmt.Println("春です。")
+        case 6, 7, 8:
+                fmt.Println("夏です。")
+        case 9, 10, 11:
+                fmt.Println("冬です。")
+        default:
+                fmt.Println("月の値ではありませんよ？")
         }
 }
