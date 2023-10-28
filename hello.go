74d6fa854f491f68d5570d498a6a3561b4348e6e
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-10-28 21:15:30 +0900

    labelとgoto

--- hello.go
+++ hello.go
@@ -7,32 +7,18 @@ import (
 )
 
 func main() {
-        x := hello.Input("type 1~5")
-        n, err := strconv.Atoi(x)
-        if err == nil {
-                fmt.Print(x + "までの合計は、")
-        } else {
-                return
-        }
         t := 0
-        switch n {
-        case 5:
-                t += 5
-                fallthrough
-        case 4:
-                t += 4
-                fallthrough
-        case 3:
-                t += 3
-                fallthrough
-        case 2:
-                t += 2
-                fallthrough
-        case 1:
-                t++
-        default:
-                fmt.Println("範囲外です。")
-                return
+        x := hello.Input("type a number")
+        n, err := strconv.Atoi(x)
+        if err != nil {
+                goto err
         }
-        fmt.Println(t, "です。")
+        for i := 1; i <= n; i++ {
+                t += i
+        }
+        fmt.Println("total:", t)
+        return
+
+err:
+        fmt.Println("ERROR!")
 }
