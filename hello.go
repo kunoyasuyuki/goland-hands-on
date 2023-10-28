e3b2b6e9f76202dac07a5d51551c0b4940737496
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-10-28 20:59:13 +0900

    fall through構文

--- hello.go
+++ hello.go
@@ -7,21 +7,32 @@ import (
 )
 
 func main() {
-        x := hello.Input("type a number")
-        fmt.Print(x + "月は、")
-        switch n, err := strconv.Atoi(x); n {
-        case 0:
-                fmt.Println("整数値が得られません。")
-                fmt.Println(err)
-        case 1, 2, 12:
-                fmt.Println("冬です。")
-        case 3, 4, 5:
-                fmt.Println("春です。")
-        case 6, 7, 8:
-                fmt.Println("夏です。")
-        case 9, 10, 11:
-                fmt.Println("冬です。")
-        default:
-                fmt.Println("月の値ではありませんよ？")
+        x := hello.Input("type 1~5")
+        n, err := strconv.Atoi(x)
+        if err == nil {
+                fmt.Print(x + "までの合計は、")
+        } else {
+                return
         }
+        t := 0
+        switch n {
+        case 5:
+                t += 5
+                fallthrough
+        case 4:
+                t += 4
+                fallthrough
+        case 3:
+                t += 3
+                fallthrough
+        case 2:
+                t += 2
+                fallthrough
+        case 1:
+                t++
+        default:
+                fmt.Println("範囲外です。")
+                return
+        }
+        fmt.Println(t, "です。")
 }
