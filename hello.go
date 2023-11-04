64e249271c9b919278d97e6a0d46f0b74ea9d55c
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-11-04 19:05:41 +0900

    構造体の導入

--- hello.go
+++ hello.go
@@ -2,15 +2,13 @@ package main
 
 import "fmt"
 
-func main() {
-	ar := []int{10, 20, 30}
-	fmt.Println(ar)
-	initial(&ar)
-	fmt.Println(ar)
+var mydata struct {
+	Name string
+	Data []int
 }
 
-func initial(ar *[]int) {
-	for i := 0; i < len(*ar); i++ {
-		(*ar)[i] = 0
-	}
+func main() {
+	mydata.Name = "Taro"
+	mydata.Data = []int{10, 20, 30}
+	fmt.Println(mydata)
 }
