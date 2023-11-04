a11b40bbeab2bb2c38c132de174197f106d837b2
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-11-04 19:28:51 +0900

    ポインタによるレシーバー

--- hello.go
+++ hello.go
@@ -1,24 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"hello"
+	"strconv"
+)
 
-// Mydata is structure.
-type Mydata struct {
-	Name string
-	Data []int
+// "hello"と"strconv"をimportする
+
+type intp int
+
+func (num intp) IsPrime() bool {
+	n := int(num)
+	for i := 2; i <= (n / 2); i++ {
+		if n%i == 0 {
+			return false
+		}
+	}
+	return true
 }
 
-// PrintData is println all data.
-func (md Mydata) PrintData() {
-	fmt.Println("*** Mydata ***")
-	fmt.Println("Name: ", md.Name)
-	fmt.Println("Data: ", md.Data)
-	fmt.Println("*** end ***")
+func (num intp) PrimeFactor() []int {
+	ar := []int{}
+	x := int(num)
+	n := 2
+	for x > n {
+		if x%n == 0 {
+			x /= n
+			ar = append(ar, n)
+		} else {
+			if n == 2 {
+				n++
+			} else {
+				n += 2
+			}
+		}
+	}
+	ar = append(ar, x)
+	return ar
+}
+
+func (num *intp) doPrime() {
+	pf := num.PrimeFactor()
+	*num = intp(pf[len(pf)-1])
 }
 
 func main() {
-	taro := Mydata{
-		"Hanako", []int{98, 76, 54, 32, 10},
-	}
-	taro.PrintData()
+	s := hello.Input("type a number")
+	n, _ := strconv.Atoi(s)
+	x := intp(n)
+	fmt.Printf("%d [%t].\n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
+	x.doPrime()
+	fmt.Printf("%d [%t].\n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
+	x++
+	fmt.Printf("%d [%t].\n", x, x.IsPrime())
+	fmt.Println(x.PrimeFactor())
 }
