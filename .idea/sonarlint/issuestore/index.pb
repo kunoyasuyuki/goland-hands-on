a8734ebf64104c3f203635ee3005b39a6b2e84e5
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-11-13 18:32:19 +0900

    非同期

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,6 @@
+
+8
+hello.go,0/e/0e76fb29ec653ebc03fcf196fa43282f144efabe
+:
+
+sample.txt,b/6/b68a6c43678e868fd7d75ff39db39eddc5559e35
--- hello.go
+++ hello.go
@@ -2,59 +2,28 @@ package main
 
 import (
 	"fmt"
-	"hello"
-	"strconv"
+	"sync"
 )
 
-// "hello"と"strconv"をimportする
-
-type intp int
-
-func (num intp) IsPrime() bool {
-	n := int(num)
-	for i := 2; i <= (n / 2); i++ {
-		if n%i == 0 {
-			return false
-		}
-	}
-	return true
-}
-
-func (num intp) PrimeFactor() []int {
-	ar := []int{}
-	x := int(num)
-	n := 2
-	for x > n {
-		if x%n == 0 {
-			x /= n
-			ar = append(ar, n)
-		} else {
-			if n == 2 {
-				n++
-			} else {
-				n += 2
-			}
-		}
-	}
-	ar = append(ar, x)
-	return ar
-}
-
-func (num *intp) doPrime() {
-	pf := num.PrimeFactor()
-	*num = intp(pf[len(pf)-1])
-}
-
 func main() {
-	s := hello.Input("type a number")
-	n, _ := strconv.Atoi(s)
-	x := intp(n)
-	fmt.Printf("%d [%t].\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
-	x.doPrime()
-	fmt.Printf("%d [%t].\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
-	x++
-	fmt.Printf("%d [%t].\n", x, x.IsPrime())
-	fmt.Println(x.PrimeFactor())
+	n := 0
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			n++
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1000; i++ {
+			n++
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println(n)
 }
