24b5bc88f6e6ca701caeca6571bd73189b881638
KunoYasuyuki <kunoyasuyuki@yahoo.co.jp>
2023-11-23 19:53:39 +0900

    http-method

--- main.go
+++ main.go
@@ -1,38 +1,20 @@
-//https://www.sunapro.com/go-interface/
 package main
 
-import "fmt"
-
-// shapeという型は必ずgetAreaメソッドを持っていることを保証している。
-type shape interface {
-	getArea() float64
-}
-
-type triangle struct {
-	height float64
-	base   float64
-}
-
-type square struct {
-	sideLength float64
-}
-
-func printArea(sh shape) {
-	fmt.Println(sh.getArea())
-}
-
-func (t triangle) getArea() float64 {
-	return 0.5 * t.base * t.height
-}
-
-func (s square) getArea() float64 {
-	return s.sideLength * s.sideLength
-}
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	t := triangle{height: 3, base: 6}
-	s := square{sideLength: 5}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-	printArea(t)
-	printArea(s)
+		switch r.Method {
+		case http.MethodGet:
+
+			fmt.Fprintf(w, "Hello World!")
+		default:
+		}
+	})
+
+	http.ListenAndServe(":8080", nil)
 }
